13c62c86a80b7bbd94783c73a029e5ea44ff1a83
Zeke Sikelianos <zeke@sikelianos.com>
2025-02-28 09:59:56 -0800

    working tsx setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import models from './models.json' assert { type: 'json' };
+import { readFile } from 'node:fs/promises';
+
+const modelsJson = await readFile(new URL('./models.json', import.meta.url), 'utf-8');
+const models = JSON.parse(modelsJson);
+
 
 
 console.log(models);
