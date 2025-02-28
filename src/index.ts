67bde980e9db03059402fd7f2a9d9d7594f8583c
Zeke Sikelianos <zeke@sikelianos.com>
2025-02-28 10:05:18 -0800

    fetch model statuses

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
-import { readFile } from 'node:fs/promises';
+import { type InferenceModel, inferenceModels } from './models.js';
 
-const modelsJson = await readFile(new URL('./models.json', import.meta.url), 'utf-8');
-const models = JSON.parse(modelsJson);
+const getModelStatus = async (model: InferenceModel) => {
+	try {
+		const [modelName, _modelVersion] = model.providerModel.split(":");
+		const modelUrl = `https://replicate.com/${modelName}/status`;
+
+		const response = await fetch(modelUrl);
+		const { status } = await response.json() as { status: string };
+
+		return status === 'online' ? 'live' : 'staging';
+	} catch {
+		return 'staging';
+	}
+};
+
+const getAllModelStatuses = async () => {
+	const statuses = await Promise.all(inferenceModels.map(getModelStatus));
+
+const models = inferenceModels.map((model, index) => ({
+    ...model,
+    status: statuses[index],
+	}));
+
+	return models;
+};
 
 
+const models = await getAllModelStatuses();
 
 console.log(models);
