1ea8ef6ce1966625cda969da3d3a9690cf157824
Zeke Sikelianos <zeke@sikelianos.com>
2025-05-05 07:18:09 -0700

    use more specific flux dev lora tag

--- src/tags.ts
+++ src/tags.ts
@@ -11,7 +11,7 @@ export const inferenceTags: InferenceTag[] = [
     {
         type: 'tag-filter',
         task: 'text-to-image',
-        tags: ['flux', 'lora'],
+        tags: ['lora', 'base_model:adapter:black-forest-labs/FLUX.1-dev'],
         providerModel: 'black-forest-labs/flux-dev-lora',
         adapterType: 'lora',
         status: 'live',
