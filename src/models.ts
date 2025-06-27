e625c17e139f5305a82ec5f6b24f7a879d0d1b5e
Luis C <me@lucataco.com>
2025-06-27 10:31:08 -0400

    Switch back to BFL

--- src/models.ts
+++ src/models.ts
@@ -96,6 +96,6 @@ export const inferenceModels: InferenceModel[] = [
     },
     {
         hfModel: "black-forest-labs/FLUX.1-Kontext-dev",
-        providerModel: "prunaai/flux-kontext-dev",
+        providerModel: "black-forest-labs/flux-kontext-dev",
     },
 ];
