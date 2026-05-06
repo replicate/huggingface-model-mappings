6fcbb3c80aa0cadf2814f8714ef003d45c70fa29
lucataco <lcatacora@cloudflare.com>
2026-05-06 14:53:57 -0400

    Add more model mappings

--- src/tags.ts
+++ src/tags.ts
@@ -18,6 +18,14 @@ export const inferenceTags: InferenceTag[] = [
     },
     {
         type: 'tag-filter',
+        task: 'text-to-image',
+        tags: ['lora', 'base_model:adapter:black-forest-labs/FLUX.1-schnell'],
+        providerModel: 'black-forest-labs/flux-schnell-lora',
+        adapterType: 'lora',
+        status: 'live',
+    },
+    {
+        type: 'tag-filter',
         task: 'image-to-image',
         tags: ['lora', 'base_model:adapter:black-forest-labs/FLUX.1-Kontext-dev'],
         providerModel: 'black-forest-labs/flux-kontext-dev-lora',
